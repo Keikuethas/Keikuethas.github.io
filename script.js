5ffd88d306e508b67290f529e7f0ae09163d75d1
Keikuethas <64356744+Keikuethas@users.noreply.github.com>
2020-04-26 22:00:42 +0300

    ну ты знаешь

---
+++ script.js
@@ -0,0 +1,48 @@
+var your = 0;
+var rbutton = document.getElementById("rbutton");
+var sbutton = document.getElementById("sbutton"); 
+var pbutton = document.getElementById("pbutton"); 
+
+function fight()
+{
+	var res = 0;
+	var my = getRandomInRange(0,2);
+	switch(your) 
+	{
+		case 0:switch(my)
+		{
+			case 0:res = 2;
+			case 1:res = 1;
+		}
+		case 1:switch(my)
+		{
+			case 2:res = 1;
+			case 1:res = 2;
+		}
+		case 2:switch(my)
+		{
+			case 0:res = 1;
+			case 2:res = 2;
+		}
+		console.log(res);
+		document.write(res);
+		Window.promt(res)
+	}
+
+function getYour(you)
+{
+	your = you;
+	fight();
+}
+
+function getRandomInRange(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+}
+rbutton.addEventListener("click",getYour(0));
+sbutton.addEventListener("click",getYour(1));
+pbutton.addEventListener("click",getYour(2));
+
