68a84aa672885f4d9f0151f13011ca97379edd9c
Keikuethas <64356744+Keikuethas@users.noreply.github.com>
2020-07-28 17:41:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,62 @@
 var your = 0;
-var rbutton = document.getElementById("rbutton");
-var sbutton = document.getElementById("sbutton"); 
-var pbutton = document.getElementById("pbutton"); 
+var rbutton = document.getElementById("r");
+var sbutton = document.getElementById("s"); 
+var pbutton = document.getElementById("p"); 
 
 function fight()
 {
 	var res = 0;
 	var my = getRandomInRange(0,2);
+	var a = '';
 	switch(your) 
 	{
 		case 0:switch(my)
 		{
-			case 0:res = 2;
-			case 1:res = 1;
+			case 0:res = 2; break;
+			case 1:res = 1; break;
 		}
+		break;
 		case 1:switch(my)
 		{
-			case 2:res = 1;
-			case 1:res = 2;
+			case 2:res = 1; break;
+			case 1:res = 2; break;
 		}
+		break;
 		case 2:switch(my)
 		{
-			case 0:res = 1;
-			case 2:res = 2;
+			case 0:res = 1; break;
+			case 2:res = 2; break;
 		}
-		console.log(res+" my is:"+my);
-		//document.write(res);
-		var a1 = Window.prompt(res)
+		break;
 	}
+	
+	switch(res) 
+	{
+		case 0:
+			sendMessage('Вы проиграли. Ничего, может быть, в следующий раз?');
+			break;
+		case 1:
+			sendMessage('Вы победили!');
+			break;
+		case 2:
+			sendMessage('Ничья!');
+			break;
+		
+	}
+	console.log('res == ' + res);
+	console.log('in cause of your == ' + your + ' and my == ' + my);
+		
+}
+
+function sendMessage(message)
+{
+	var a = window.prompt(message);
+}
+
+function getYour(you)
+{
+	your = you;
+	fight();
 }
 
 function getRandomInRange(min, max)
@@ -37,17 +66,20 @@ function getRandomInRange(min, max)
 
 rbutton.addEventListener("click",function()
 {
-your = 0;
-fight();
+	your = 0;
+	fight();
 });
+
 sbutton.addEventListener("click",function()
 {
-your = 1;
-fight();
+	your = 1;
+	fight();
 });
+
 pbutton.addEventListener("click",function()
 {
-your = 2;
-fight();
+	your = 2;
+	fight();
 });
+
 
